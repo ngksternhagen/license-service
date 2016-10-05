42104c137ebe02416c7089c4aa55c1e5ba1a401d
NGK Sternhagen <nsternhagen@gmail.com>
2016-10-05 09:14:59 -0700

    provision application environment using Centos 7

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,10 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "centos/7"
   config.vm.box_check_update = true
+  config.ssh.insert_key = false
   config.vm.network "forwarded_port", guest: 80, host: 8888 
+  config.vm.network "private_network", ip: "192.168.50.4"
   config.vm.provision 'shell', path: './provision.sh'
 end
--- provision.sh
+++ provision.sh
@@ -1,4 +1,11 @@
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y python-pip
+#!/bin/bash
+sudo yum -y update
+
+# must enable EPEL repo to get pip
+sudo yum -y install wget
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+rpm -ivh epel-release-7-8.noarch.rpm
+
+sudo yum --enablerepo=epel -y install python-pip
 sudo pip install flask-restful
+
