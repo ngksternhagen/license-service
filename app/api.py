6946a306a4106688220a427f41031ca0d1bf17a1
NGK Sternhagen <nsternhagen@gmail.com>
2016-10-07 08:22:55 -0700

    add GPL3 resource

--- app/api.py
+++ app/api.py
@@ -6,11 +6,12 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class GPL3(Resource):
     def get(self):
-        return {'hello': 'world'}
+        body = open('../licenses/gnu-gpl-version-3')
+        return body
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(GPL3, '/GPL3/')
 
 if __name__ == '__main__':
     app.run(debug=True)
